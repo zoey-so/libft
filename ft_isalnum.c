e6f82ac58a66d84d45905b88cb1aaf9b4e91661d
zoey-so <xkokaxx@gmail.com>
2026-06-16 21:45:44 +0200

    isalnum isdigit

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -10,4 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_isalnum(int c)
+{
+	if (ft_isdigit(c) || ft_isalpha(c))
+		return (1);
+	return (0);
+}
