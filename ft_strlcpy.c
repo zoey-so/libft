09baba5686833b567e644fa9972e41d1a1a17848
zoey-so <xkokaxx@gmail.com>
2026-06-16 22:39:06 +0200

    ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -10,4 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	const char	*src_cp;
+	size_t		left_size;
+
+	src_cp = src;
+	left_size = size;
+	if (left_size)
+	{
+		while (--left_size)
+		{
+			*(dest++) = *(src++);
+			if (*src == '\0')
+			{
+				*dest++ = *src++;
+				break ;
+			}
+		}
+	}
+	if (!left_size)
+	{
+		if (size)
+			*dest = '\0';
+		while (*src++)
+			;
+	}
+	return (src - src_cp - 1);
+}
