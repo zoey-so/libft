05866487d13279ed20a1eb8cdb6f64b60c0acd2d
zoey-so <xkokaxx@gmail.com>
2026-06-16 15:53:36 +0200

    memcmp done

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -10,4 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	while (n)
+	{
+		if (*s1++ != *s2++)
+			return (*--s1 - *--s2);
+		n--;
+	}
+	return (0);
+}
