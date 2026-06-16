35d4954c6ebfe3ecc51afb979541aa21afffae5e
zoey-so <xkokaxx@gmail.com>
2026-06-16 22:41:22 +0200

    ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -10,4 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	if (n)
+	{
+		while (*s1 == *s2 && --n > 0 && *s1)
+		{
+			s1++;
+			s2++;
+		}
+		return ((int)((unsigned char)*s1 - (unsigned char)*s2));
+	}
+	return (0);
+}
