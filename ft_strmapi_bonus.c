f15bec6788caa8e9fb1ec654a05fe1aa4e623e49
zoey-so <xkokaxx@gmail.com>
2026-06-19 20:22:10 +0200

    now strmapi...

---
+++ ft_strmapi_bonus.c
@@ -0,0 +1,24 @@
+#include "libft_bonus.h"
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char *res;
+	size_t len;
+	unsigned int i;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		res[i] = f(i, s[i]);
+		i++;
+	}
+	res[len] = '\0';
+	return (res);
+}
