4c79608df6e7041af5084401fe297dbddb7e7224
zoey-so <xkokaxx@gmail.com>
2026-06-19 17:15:41 +0200

    strtrim

---
+++ ft_strtrim_bonus.c
@@ -0,0 +1,24 @@
+#include "libft_bonus.h"
+#include "libft.h"
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+	char		*res;
+	const char	*beg;
+	const char	*end;
+
+	if (!s1)
+		return (NULL);
+	if (!set || !*set || !*s1)
+		return (ft_strdup(s1));
+	beg = s1;
+	while (ft_strchr(set, *beg))
+		beg++;
+	if (*beg == '\0')
+		return (ft_strdup("\0"));
+	end = s1 + ft_strlen(s1) - 1;
+	while (end > beg && ft_strchr(set, *end))
+		end--;
+	res = ft_substr(beg, 0, end - beg + 1);
+	return (res);
+}
