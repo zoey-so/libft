129ef6ebee3a30a6a81e2b0818334a6cb0a0a0b3
zoey-so <xkokaxx@gmail.com>
2026-06-19 21:10:24 +0200

    putnbrfd

---
+++ ft_putnbr_fd_bonus.c
@@ -0,0 +1,19 @@
+#include "libft_bonus.h"
+
+void ft_putnbr_fd(int n, int fd)
+{
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		if (n == -2147483648)
+		{
+			ft_putstr_fd("2147483648", fd);
+			return ;
+		}
+		else
+			n *= -1;
+	}
+	if (n > 9)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd('0' + (n % 10), fd);
+}
