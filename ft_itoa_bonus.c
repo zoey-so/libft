7b64a99053ab7a9e9d8f3ddf0c3d9044d41f2cb6
zoey-so <xkokaxx@gmail.com>
2026-06-19 20:01:13 +0200

    itoa

---
+++ ft_itoa_bonus.c
@@ -0,0 +1,31 @@
+#include "libft_bonus.h"
+#include "libft.h"
+#include <limits.h>
+
+char *ft_itoa(int n)
+{
+	char	*res;
+	long int	nb;
+	size_t	len;
+
+	len = 0;
+	nb = (((long int)n >> 31) ^ ((long int)n)) - ((long int)n >> 31);
+	while (++len && nb > 9)
+		nb /= 10;
+	if (n < 0)
+		len++;
+	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	res[len] = '\0';
+	nb = (((long int)n >> 31) ^ ((long int)n)) - ((long int)n >> 31);
+	while (len-- > 0)
+	{
+		res[len] = '0' + (nb % 10);
+		nb /= 10;
+	}
+	if (n < 0)
+		res[0] = '-';
+	return (res);
+}
+
