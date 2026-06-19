2f863826a8dc7aad0ea276cbf89776441bab5caa
zoey-so <xkokaxx@gmail.com>
2026-06-19 16:53:21 +0200

    strjoin, substr

---
+++ ft_strjoin_bonus.c
@@ -0,0 +1,19 @@
+#include "libft_bonus.h"
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len_s1;
+	size_t	len_s2;
+	char	*res;
+
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	res = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	if (!res)
+		return (NULL);
+	ft_memcpy(res, s1, len_s1);
+	ft_memcpy(res + len_s1, s2, len_s2);
+	res[len_s1 + len_s2] = '\0';
+	return (res);
+}
---
+++ ft_substr_bonus.c
@@ -0,0 +1,22 @@
+#include "libft_bonus.h"
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*res;
+	size_t	s_len;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup("\0"));
+	if (s_len - start < len)
+		len = s_len - start;
+	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	ft_memcpy(res, s + start, len);
+	res[len] = '\0';
+	return (res);
+}
