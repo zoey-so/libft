0c93607c6637bfe072ef925851011857971a169f
zoey-so <xkokaxx@gmail.com>
2026-06-20 17:44:22 +0200

    lst last

--- libft.h
+++ libft.h
@@ -44,4 +44,6 @@ int		ft_toupper(int c);
 char	*ft_strchr(const char *p, int ch);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 #endif
